3e000899db1d8bfcbc5bb253185b99016b897d65
Mitt <mittcoats@gmail.com>
2015-10-07 20:27:42 +0000

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,30 +18,42 @@ class User < ActiveRecord::Base
     BCrypt::Password.create(string, cost: cost)
   end
    
-   # Returns random token
-   def User.new_token
-     SecureRandom.urlsafe_base64
-   end
-
-   # Remembers a user in the database for use in persistent sessions.
-   def remember
-     self.remember_token = User.new_token
-     update_attribute(:remember_digest, User.digest(remember_token))
-   end
-   
-   # Returns true if given token matches digest.
-   def authenticated?(remember_token)
-     return false if remember_digest.nil?
-     BCrypt::Password.new(remember_digest).is_password?(remember_token)
-   end
-   
-   # Forgets a user.
-   def forget
-     update_attribute(:remember_digest, nil)
-   end
-   
-   private
-   # Convert email to all lower-case.
+  # Returns random token
+  def User.new_token
+   SecureRandom.urlsafe_base64
+  end
+  
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+   self.remember_token = User.new_token
+   update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+  
+  # Forgets a user.
+  def forget
+   update_attribute(:remember_digest, nil)
+  end
+  
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+  
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+  
+  private
+    # Convert email to all lower-case.
     def downcase_email
       self.email = email.downcase
     end
